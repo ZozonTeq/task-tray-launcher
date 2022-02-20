214d600b2af4575467e0e68a59fd9fd9464a4823
ZozonTeq <otto1104t@gmail.com>
2022-02-21 08:28:18 +0900

    プロジェクト ファイルを追加します。

---
+++ ShortCuts/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ShortCuts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            this.contextMenuStrip1.Closed += new System.Windows.Forms.ToolStripDropDownClosedEventHandler(this.contextMenuStrip1_Closed);
+            this.contextMenuStrip1.Opened += new System.EventHandler(this.contextMenuStrip1_Opened);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.testToolStripMenuItem.Text = "終了";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+    }
+}
+
---
+++ ShortCuts/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortCuts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            if (!Directory.Exists("files")) Directory.CreateDirectory("files");
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Visible = true;
+        }
+
+        private void contextMenuStrip1_Closed(object sender, ToolStripDropDownClosedEventArgs e)
+        {
+            contextMenuStrip1 = new ContextMenuStrip();
+            contextMenuStrip1.Opened += contextMenuStrip1_Opened;
+            contextMenuStrip1.Items.Add("Exit").Name="Exit";
+            contextMenuStrip1.Items.Add("Reload").Name = "Reload";
+            contextMenuStrip1.Items.Add("Open").Name = "Open";
+        }
+
+        private void contextMenuStrip1_Opened(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Items.Clear();
+            string[] files = Directory.GetFiles("files");
+            for (int i = 0; i < files.Length; i++)
+            {
+                contextMenuStrip1.Items.Add(files[i]);
+                contextMenuStrip1.Items[i].Name = files[i];
+                contextMenuStrip1.Items[i].Text = files[i];
+                contextMenuStrip1.Items[i].Click += OnPressFile;
+            }
+
+            contextMenuStrip1.Items.Add("Exit").Name = "Exit";
+            contextMenuStrip1.Items.Find("Exit", false)[0].Text = "Exit";
+            contextMenuStrip1.Items.Find("Exit", false)[0].Click += OnPressExit;
+
+            contextMenuStrip1.Items.Add("Reload").Name = "Reload";
+            contextMenuStrip1.Items.Find("Reload", false)[0].Text = "Reload";
+            contextMenuStrip1.Items.Find("Reload", false)[0].Click += OnPressReload;
+
+            contextMenuStrip1.Items.Add("Open").Name = "Open";
+            contextMenuStrip1.Items.Find("Open", false)[0].Text = "Open";
+            contextMenuStrip1.Items.Find("Open", false)[0].Click += OnPressOpen;
+            foreach (ToolStripItem o in contextMenuStrip1.Items)
+            {
+                Console.WriteLine(o.Name);
+            }
+        }
+        private void OnPressExit(object sender,EventArgs e)=> this.Close();
+        private void OnPressFile(object sender,EventArgs e)
+        {
+            string ext = Path.GetExtension(sender.ToString());
+            if(ext == ".lnk")
+            {
+                bool error = false;
+                IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShell();
+                IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(sender.ToString());
+                string targetPath = shortcut.TargetPath.ToString();
+                try
+                {
+                    System.Diagnostics.Process.Start(targetPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    error = true;
+                }
+                if (!error)
+                {
+                    Console.WriteLine($"Executing {targetPath}...");
+                }
+                else
+                {
+                    try
+                    {
+                        System.Diagnostics.Process.Start(targetPath.Replace("C:\\Program Files (x86)\\", "C:\\Program Files\\"));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine($"==========\nERROR FOUND\nwhile executing {targetPath.Replace("C:\\Program Files (x86)\\", "C:\\Program Files\\")}");
+                        error = true;
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(sender.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }//通常の実行
+        }
+        private void OnPressOpen(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("EXPLORER.EXE", $@"/select,""files""");
+            Console.WriteLine($"{Directory.GetCurrentDirectory() + "\\files\\"}");
+        }
+        private void OnPressReload(object sender,EventArgs e)
+        {
+            Application.Restart();
+        }
+
+    }
+}
